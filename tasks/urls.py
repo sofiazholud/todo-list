e641bceee2a3633de8d8e3883341a3aa0518e82d
Sofiia Zholud <szholuds@gmail.com>
2024-10-30 11:53:01 +0200

    Initial commit for TODO list project with migrations and template updates

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.task_list, name='task_list'),
-    path('create/', views.task_create, name='task_create'),
-    path('update/<int:pk>/', views.task_update, name='task_update'),
-]+[
+    path('task/create/', views.task_create, name='task_create'),
+    path('task/update/<int:pk>/', views.task_update, name='task_update'),
+    path('task/delete/<int:pk>/', views.task_delete, name='task_delete'),
     path('tags/', views.tag_list, name='tag_list'),
+    path('tag/create/', views.tag_create, name='tag_create'),
+    path('tag/update/<int:pk>/', views.tag_update, name='tag_update'),
+    path('tag/delete/<int:pk>/', views.tag_delete, name='tag_delete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 # tasks/views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task, Tag
 from .forms import TaskForm, TagForm
 
@@ -29,6 +29,11 @@ def task_update(request, pk):
         form = TaskForm(instance=task)
     return render(request, 'tasks/task_form.html', {'form': form})
 
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('task_list')
+
 def tag_list(request):
     tags = Tag.objects.all()
     return render(request, 'tasks/tag_list.html', {'tags': tags})
@@ -42,3 +47,21 @@ def tag_create(request):
     else:
         form = TagForm()
     return render(request, 'tasks/tag_form.html', {'form': form})
+
+def tag_update(request, pk):
+    tag = get_object_or_404(Tag, pk=pk)
+    if request.method == 'POST':
+        form = TagForm(request.POST, instance=tag)
+        if form.is_valid():
+            form.save()
+            return redirect('tag_list')
+    else:
+        form = TagForm(instance=tag)
+    return render(request, 'tasks/tag_form.html', {'form': form})
+
+def tag_delete(request, pk):
+    tag = get_object_or_404(Tag, pk=pk)
+    if request.method == 'POST':
+        tag.delete()
+        return redirect('tag_list')
+    return render(request, 'tasks/tag_confirm_delete.html', {'tag': tag})
